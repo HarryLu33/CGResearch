7f194068c67d7fc07fc80e826b10acce9cb891ae
HarryLu <harrry.lu@tennentgroup.com.au>
2024-05-27 16:15:20 +1000

    test

--- laplacian_mesh_operation.py
+++ laplacian_mesh_operation.py
@@ -2,6 +2,7 @@ import statistics
 
 import torch
 from pytorch3d.io import load_obj, IO
+from pytorch3d.ops import cot_laplacian
 from pytorch3d.structures import Meshes
 from get_neighbours_utils import get_all_neighbours
 from hc_utils import hc_algorithm
@@ -28,35 +29,41 @@ def laplacian_operation(filename, option, weight, us_HC: bool, B, turns):
     num_verts = verts.shape[0] if isinstance(verts, torch.Tensor) else len(verts)
 
     # load vertices neighbour relations
-    all_neighbours_indexes = None
-    if (option == "optimization") | us_HC:
-        all_neighbours_indexes = get_all_neighbours(verts, meshes.edges_packed())
+    # all_neighbours_indexes = None
+    # if (option == "optimization") | us_HC:
+    #     all_neighbours_indexes = get_all_neighbours(verts, meshes.edges_packed())
 
     t = 1
     while t <= turns:
         # get the Laplacian matrix for the whole mesh
-        L_u = meshes.laplacian_packed().to_dense()
-        # use uniform weights W_p = I
-        I_m = torch.eye(num_verts)
-        # concatenate L and W_p to get A, use W_l = I
-        A = torch.cat((L_u, I_m), dim=0)
-
-        f = None
-        # set f = delta_dc in detail preserving optimization
-        if option == "optimization":
-            # use cotangent weight
-            delta_dc = laplacian_coordinates(verts, all_neighbours_indexes, weight)
-            f = delta_dc
-        # set f = 0 in mesh smoothing
-        elif option == "smoothing":
-            zero_matrix = torch.zeros(num_verts, 3)
-            f = zero_matrix
+        if weight == "constant":
+            L = meshes.laplacian_packed().to_dense()
+        else:
+            L, inv_areas = cot_laplacian(meshes.verts_packed(), meshes.faces_packed())
+            L = L.to_dense()
 
         x = verts[:, 0]
         y = verts[:, 1]
         z = verts[:, 2]
         # use all vertices as anchors in V_d
         V_d = torch.stack((x, y, z), dim=1)
+
+        f = None
+        # set f = delta_dc in detail preserving optimization
+        if option == "optimization":
+            # use cotangent weight
+            delta_dc = L @ V_d
+            f = delta_dc
+        # set f = 0 in mesh smoothing
+        elif option == "smoothing":
+            zero_matrix = torch.zeros(num_verts, 3)
+            f = zero_matrix
+
+        # use uniform weights W_p = I
+        I_m = torch.eye(num_verts)
+
+        # concatenate L and W_p to get A, use W_l = I
+        A = torch.cat((L, I_m), dim=0)
         # concatenate f and V_d to get A, use W_l = I, W_p = I
         b = torch.cat((f, V_d), dim=0)
 
@@ -65,7 +72,15 @@ def laplacian_operation(filename, option, weight, us_HC: bool, B, turns):
 
         if us_HC:
             # use HC algorithm to push the modified points back towards the previous point
-            final_verts_hc = hc_algorithm(verts, new_verts, all_neighbours_indexes, B)
+            if weight == "constant":
+                L = meshes.laplacian_packed().to_dense()
+            else:
+                L = cot_laplacian(meshes.verts_packed(), meshes.faces_packed())[0].to_dense()
+            b_i = new_verts - verts
+            A = torch.cat((L, I_m), dim=0)
+            b = torch.cat((f, b_i), dim=0)
+            b_j_mean = torch.inverse(A.t() @ A) @ A.t() @ b
+            final_verts_hc = new_verts - (B * b_i + (1 - B) * b_j_mean)
             meshes = Meshes(verts=[final_verts_hc], faces=[faces.verts_idx])
             save_to = folder + "/" + option + "/" + filename + "_" + option + "_hc_" + str(
                 B) + "_" + weight + "_" + str(t) + extension
