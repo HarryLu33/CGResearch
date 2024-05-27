becdd5f537f6bf58a6ee4f09585da5c876b62ab5
HarryLu <harrry.lu@tennentgroup.com.au>
2024-05-27 18:38:48 +1000

    update

--- get_neighbours_utils.py
+++ get_neighbours_utils.py
@@ -1,23 +1,13 @@
-def get_neighbour_vertex(edges, index):
-    vi = []
-    for edge in edges:
-        if edge[0] == index:
-            vi.append(edge[1])
-        elif edge[1] == index:
-            vi.append(edge[0])
-    return vi
-
-
-def get_all_neighbours(verts, edges):
+def get_all_neighbours(verts, L):
     neighbour_dict = {}
     i = 0
     while i < len(verts):
         vi = []
-        for edge in edges:
-            if edge[0] == i:
-                vi.append(edge[1].item())
-            elif edge[1] == i:
-                vi.append(edge[0].item())
+        j = 0
+        while j < len(verts):
+            if (L[i][j] != 0) and (L[i][j] != -1):
+                vi.append(j)
+            j += 1
         neighbour_dict[i] = vi
         i += 1
     return neighbour_dict
--- radius_ratio.py
+++ radius_ratio.py
@@ -38,22 +38,10 @@ def radius_ratio(A: ndarray, B: ndarray, C: ndarray):
     # Calculate the circumradius
     circumradius = (a * b * c) / (4 * area)
 
-    # Calculate the circumcenter
-    # Using the formula for circumcenter in 3D space
-    D = 2 * (np.linalg.norm(np.cross(B - A, C - A)) ** 2)
-    Ux = (np.dot(np.cross(B - A, B - C), np.cross(B - A, C - A)) * np.dot(C - A, C - A) +
-          np.dot(np.cross(C - A, C - B), np.cross(C - A, A - B)) * np.dot(A - B, A - B) +
-          np.dot(np.cross(A - B, A - C), np.cross(A - B, B - C)) * np.dot(B - C, B - C)) / D
-    circumcenter = A + Ux * (B - A)
-
     # Calculate the semi-perimeter
     s = (a + b + c) / 2
 
     # Calculate the inradius
     inradius = area / s
-
-    # Calculate the incenter
-    # Using the formula for incenter in 3D space
-    incenter = (a * A + b * B + c * C) / (a + b + c)
 
     return 2 * inradius / circumradius
